88f701f3332c3943307074cae62834e3c00489c5
Homiepq <andronews8i@gmail.com>
2025-11-13 21:08:14 +0200

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,99 @@
 const express = require('express')
+const multer = require('multer')
+const path = require('path')
+
+const db = require('./db')
+const { title } = require('process')
+
 const app = express()
 const port = 3000
 
 app.use(express.static("static"))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", "ejs")
+app.set("views", "views")
 
 let products = []
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage: storage})
+
 app.get('/', (req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.send("Hello World!")
+    db.query("SELECT * FROM products", (err, rows) => {
+        let products = rows
+
+        products.forEach((product) => {
+            product.image = JSON.parse(product.image)
+        })
+        res.render("index", {products})
+    })
 })
 
-app.post("/add", (req, res) => {
+app.post("/add", upload.fields([{name: "image"}]), (req, res) => {
     let data = req.body
-    products.push(data)
-    res.status(200)
-    res.redirect("/")
+    console.log(data)
+    data.image = req.files.image.map((file) => file.filename)
+    data.image = JSON.stringify(data.image)
+    db.query("INSERT INTO products SET ?", data, (err) => { 
+    res.status(201)
+    res.end()  
+    })
 })
 
-app.post("/ads", (req, res) => {
-    res.status(200)
-    res.setHeader("Content-Type", "application/json")
-    res.json(products).end()
+app.get("/post/:id", (req, res,) =>{
+    const postId = req.params.id
+
+    db.query(
+        `
+        SELECT p.*, c.id AS commentId, c.author, c.comment
+        FROM products p
+        LEFT JOIN comments c ON c.post_id = p.id
+        WHERE p.id = ?`,
+        postId,
+        (err, rows) => {
+            if (err || rows.length === 0) return res.status(404).render("notfound")
+            let product = {
+                id: rows[0].id,
+                title: rows[0].title,
+                description: rows[0].description,
+                image: JSON.parse(rows[0].image),
+                comments: rows.map((row) => {
+                    return {
+                        id: row.commentId,
+                        author: row.author,
+                        comment: row.comment
+                    }
+                })
+            }
+            console.log(product)
+            res.render("post", { product } )
+        }
+
+    )
+})
+
+app.post("/comment", (req, res) => {
+    let data = req.body
+    db.query("INSERT INTO comments SET ?", data, (err) => {
+        res.status(201)
+        res.end()
+    })
 })
 
 app.use((req, res, next) => {
     res.status(404)
-    res.sendFile("notFound.html", {root: "static"})
+    res.render("notfound")
 })
 
 app.listen(port, () => {
---
+++ db.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql2")
+
+require("dotenv").config()
+
+let host = process.env.HOST_url
+let port = process.env.PORT
+let user = process.env.DB_USER
+let password = process.env.PASSWORD
+let dbname = process.env.DATABASE_NAME
+
+const connection = mysql.createConnection({
+    host,
+    user,
+    password,
+    port,
+    database: dbname
+})
+
+connection.connect(async (err) => {
+    if (err) {
+        console.error("Error connecting to database:" + err.stack)
+        return
+    }
+})
+
+module.exports = connection 
